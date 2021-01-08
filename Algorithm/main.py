72e5c89087f4fff4d825dd39c2f841164bd4c751
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2021-01-07 23:07:19 -0500

    changed naming convention and made rgb array function

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -18,93 +18,109 @@ vary (find way to get screen size in app in order to accurately resize picture)?
 class Algorithm:
 
     # initializes class attributes / paths
-    def __init__(self, pic_path: str) -> None:
+    def __init__(self, path: str) -> None:
         try:
-            self.__pic_path, self.__img = pic_path, Image.open(pic_path)
-            self.__grayscale_img = None
-            self.__grayscale_path = "grayscale_" + pic_path
+            self.__picPath, self.__img = path, Image.open(path)
+            self.__grayscaleImage = None
+            self.__grayscalePath = "grayscale " + path
         except FileNotFoundError:
-            self.__img = self.__grayscale_img = None
+            self.__img = self.__grayscaleImage = None
             print("File not found")
 
-    def convert_to_grayscale(self) -> None:
+    def convertToGrayscale(self) -> None:
         # checks if we have a valid source image and edited image exists in current directory
-        if self.__img and not os.path.isfile(self.__grayscale_path):
-            self.__grayscale_img = self.__img.convert("LA")
-            # self.__grayscale_img = self.__grayscale_img.convert("L")
-            self.__grayscale_img.save(self.__grayscale_path)
+        if self.__img and not os.path.isfile(self.__grayscalePath):
+            self.__grayscaleImage = self.__img.convert("LA")
+            # self.__grayscaleImage = self.__grayscaleImage.convert("L")
+            self.__grayscaleImage.save(self.__grayscalePath)
 
-    def convert_to_binary(self):
+    def convertToBinary(self):
         pass
 
-    def display_original_image(self) -> None:
+    def displayOriginalImage(self) -> None:
         try:
             plt.imshow(self.__img)
             plt.show()
         except TypeError:
             print("No image to display")
 
-    def display_edited_image(self) -> None:
+    def displayEditedImage(self) -> None:
         # displays edited image if one has already been created
         try:
-            # self.convert_to_grayscale()   # creates modifiable image if it doesn't already exist
-            plt.imshow(self.__grayscale_img)
+            # self.convertToGrayscale()   # creates modifiable image if it doesn't already exist
+            plt.imshow(self.__grayscaleImage)
             plt.show()
         except TypeError:
             print("No image to display")
 
-    def get_original_image_object(self) -> object:
-        return self.__img
-
-    def get_edited_image_object(self) -> object:
-        return self.__grayscale_img
+    # def getOriginalObject(self) -> object:
+    #     return self.__img
+    # 
+    # def getEditedObject(self) -> object:
+    #     return self.__grayscaleImage
 
     def resize(self, width: float, height: float) -> None:
         pass
 
-    def get_original_num_array(self) -> np.ndarray:
-        if self.__img:
-            return np.array(self.__img)
+    # returns 2D array where each rgb pixel is represented by a string
+    def getOriginalRGB(self) -> list:
+        if self.__img:  # checks for valid image
+            rgbArr = []
+            numArr = np.array(self.__img)   # convert imgage object to numpy array
+
+            for i in range(self.getOriginalDimensions()[0]):
+                rgbArr.append([])
+                for j in range(self.getOriginalDimensions()[1]):
+
+                    pixel = np.array(numArr[i][j]).tolist()
+                    for k in range(len(pixel)):
+                        pixel[k] = str(pixel[k])
+                    pixelString = ','.join(pixel)
+
+                    rgbArr[i].append(pixelString)
+            return rgbArr
         else:
             print("Image does not exist")
 
-    def get_edited_num_array(self) -> np.ndarray:
-        if self.__grayscale_img:
-            return np.array(self.__grayscale_img)
+    # returns 2D array where each rgb pixel is represented by a string
+    def getEditedRGB(self) -> np.ndarray:
+        if self.__grayscaleImage:
+            return np.array(self.__grayscaleImage)
         else:
-            # self.convert_to_grayscale()   # creates modifiable image if it doesn't already exist
+            # self.convertToGrayscale()   # creates modifiable image if it doesn't already exist
             print("Edited image does not exist")
 
-    def get_original_dimensions(self) -> tuple:
+    def getOriginalDimensions(self) -> tuple:
         if self.__img:
             return np.array(self.__img).shape
         else:
             print("Image does not exist")
 
-    def get_edited_dimensions(self) -> tuple:
-        if self.__grayscale_img:
-            return np.array(self.__grayscale_img).shape
+    def getEditedDimensions(self) -> tuple:
+        if self.__grayscaleImage:
+            return np.array(self.__grayscaleImage).shape
         else:
-            # self.convert_to_grayscale()   # creates modifiable image if it doesn't already exist
+            # self.convertToGrayscale()   # creates modifiable image if it doesn't already exist
             print("Edited image does not exist")
 
     # so far only displays outer edge
-    def display_edges(self):
+    def displayOutline(self):
         edge = self.__img.filter(ImageFilter.FIND_EDGES)
         plt.imshow(edge)
         plt.show()
 
-    def delete_edited_image(self) -> None:
+    def deleteEditedImage(self) -> None:
         try:
-            os.remove(self.__grayscale_path)
-            self.__grayscale_img = None
+            os.remove(self.__grayscalePath)
+            self.__grayscaleImage = None
             print("Image deleted")
         except FileNotFoundError:
-            print(f"Could not find the file {self.__grayscale_path} to delete")
+            print(f"Could not find the file {self.__grayscalePath} to delete")
 
 
 test = Algorithm("dog.png")
-test.display_original_image()
+print(test.getOriginalRGB())
+
 # if not os.path.isfile("test.txt"):
 #     compressedArr = test.get_original_num_array()[:, :, 0]
 #     np.savetxt("test.txt", compressedArr, fmt="%d", delimiter=",")
