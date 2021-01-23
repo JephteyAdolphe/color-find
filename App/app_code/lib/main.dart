aeccd373d1ab995334cb5547c658f5368379844b
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2021-01-23 17:29:38 -0500

    progress on prototype

--- App/app_code/lib/main.dart
+++ App/app_code/lib/main.dart
@@ -1,113 +1,247 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// Main app where everything will be displayed
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Color Find Prototype',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+
+        // Home Page contains everything in it (side menu, canvas, colors)
+        home: HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Color Find"),
+          centerTitle: true,
         ),
+
+        // drawer is a hamburger menu and TabList is the list of items (of type Drawer) that we create
+        drawer: TabList(),
+
+        // Body contains a list(children) of widgets which are the drawing canvas and color pallette, in that order
+        body: Column(
+          children: <Widget>[
+            DrawingBlock(),
+            ColorPalette(),
+          ],
+        ));
+  }
+}
+
+// Drawing block is where the coloring takes place
+class DrawingBlock extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 650,
+        decoration: BoxDecoration(
+            border: Border.all(width: 8),
+            borderRadius: BorderRadius.circular(12)),
+
+        // Color block is wrapped in a Container then scaffold to control the size of colors and scrolling ability
+        child: Scaffold(
+          body: ColorBlock(),
+        ));
+  }
+}
+
+// Scrollable color pallette
+class ColorPalette extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 60,
+
+        // color buttons wrapped in listview so that we can scroll through them
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.red;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.orange;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.yellow;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.green;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.blue;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.indigo;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.purple;
+                }))),
+            ElevatedButton(
+                onPressed: null,
+                child: null,
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.blue;
+                  return Colors.pink;
+                }))),
+          ],
+        ));
+  }
+}
+
+// Hamburger Menu where we list our action buttons
+class TabList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: 120,
+            child: DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text('New Image'),
+            onTap: () {
+              // Go to page
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Restart'),
+            onTap: () {
+              // Go to page
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Save'),
+            onTap: () {
+              // Go to page
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Show Outline'),
+            onTap: () {
+              // Go to page
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Show Original'),
+            onTap: () {
+              // Go to page
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ColorBlock extends StatelessWidget {
+  // Reuse this base widget on every page
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: MyPainter(),
+      child: Container(),
+      // size: Size(500, 300),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 5
+      ..strokeCap = StrokeCap.round;
+
+    Offset start = Offset(0, size.height / 2);
+    Offset end = Offset(size.width, size.height / 2);
+
+    canvas.drawLine(start, end, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter old) {
+    return false;
   }
 }
