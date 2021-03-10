47b828505e9d3936c073a2aa30c9564f19a35ec2
Kenny K. Bui <Kenny.Bui@rutgers.edu>
2021-03-10 00:52:38 -0500

    editing Prototype
    Added some print functions for testing.
    Added some more checks.

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from PIL import Image, ImageFilter
-
+import copy
 
 class Algorithm:
 
@@ -28,9 +28,9 @@ class Algorithm:
         # checks if we have a valid source image and edited image exists in current directory
         if self.__img and not os.path.isfile(self.__grayscalePath):
             if ".png" in self.__grayscalePath:
-                self.__grayscaleImage = self.__img.convert("LA")     # converts png image to grayscale
+                self.__grayscaleImage = self.__img.convert("LA")  # converts png image to grayscale
             else:
-                self.__grayscaleImage = self.__img.convert("L")     # converts jpeg image to grayscale
+                self.__grayscaleImage = self.__img.convert("L")  # converts jpeg image to grayscale
 
             self.__grayscaleImage.save(self.__grayscalePath)
 
@@ -62,7 +62,7 @@ class Algorithm:
 
     # returns 2D array where each rgb pixel is represented by a string
     def getEditedRGB(self) -> list:
-        if self.__grayscaleImage:   # checks for valid image
+        if self.__grayscaleImage:  # checks for valid image
             return self.__rgb(self.__grayscaleImage, self.getEditedDimensions()[0], self.getEditedDimensions()[1])
         else:
             # self.convertToGrayscale()   # creates modifiable image if it doesn't already exist
@@ -90,52 +90,136 @@ class Algorithm:
 
     # return color ID array for edited image
     def getEditedIDArray(self) -> list:
-        if self.__grayscaleImage:   # checks for valid image
+        if self.__grayscaleImage:  # checks for valid image
             return self.__getID(self.__grayscaleImage, self.getEditedDimensions()[0], self.getEditedDimensions()[1])
         else:
             # self.convertToGrayscale()   # creates modifiable image if it doesn't already exist
             print("Edited image does not exist")
 
     # returns 2D array where each rgb pixel is represented by an id number dictating which color block it belongs to
+
+    def getLayerNum(self, givenPixel, targets,
+                    idMap, idArr):  # given list of nearby layerNums find similar ones or return new layerNum
+        algo_color_range = 60
+        layerNumResult = -1  # return result if it's -1, means new layer
+        for i in targets:
+            pixel = idMap[i][-1]
+            matches = 0
+            for colorLayer in range(len(pixel)):
+                if abs(givenPixel[colorLayer] - pixel[colorLayer]) < algo_color_range:
+                    matches += 1
+            if matches == len(pixel):  # if the given pixel matches a idMap color:
+                if layerNumResult == -1:  # set it to idMap LayerNum
+                    layerNumResult = i
+                elif layerNumResult > i:  # if Previous layerNum is bigger use the smaller one.
+                    idMap[layerNumResult][0] = i  # tell layerNumResult to collapse to i
+                    layerNumResult = i
+                elif layerNumResult < i:  # if Previous layerNum is bigger use the smaller one.
+                    idMap[i][0] = layerNumResult  # tell i to collapse to layerNumResult
+
+        if layerNumResult == -1:  # if no matches were made give a new layerNum
+            returnLen = len(idMap)
+            idMap[returnLen] = [-1, givenPixel]
+            return [returnLen, idMap]
+        return [layerNumResult, idMap]
+
+    def travesal(self, idMap, id):
+        if idMap[id][0] == -1:
+            return id
+        else:
+            return self.travesal(idMap, idMap[id][0])
+
+    def collapse_refit(self, idMap, idArr):
+        # Collapse
+        unused_layers = []
+        for id in idMap:
+            if idMap[id][0] == -1:
+                continue
+            else:
+                unused_layers.append(id)
+                idMap[id][0] = self.travesal(idMap,idMap[id][0])
+
+        # refit [0,1,2,3,4,5,6,7,8,9,10]
+        # unused_layers [list of nums] (sorted Lowest to highest) [3,5,7,9]
+        num_unused = len(unused_layers)
+        unused_index = unused_layers.pop(0)
+        end_index = len(idMap) - 1
+        refited_items = {} # making sure to correct items
+        while end_index > unused_index:  ## swap bigger index items to a smaller one
+            if idMap[end_index][0] == -1:  ## swap only if it isn't being swapped already
+                if end_index in refited_items:
+                    for collapseTarget in refited_items[end_index]:
+                        idMap[collapseTarget][0] = unused_index
+                    del refited_items[end_index]
+                idMap[end_index][0] = unused_index
+                idMap[unused_index][1] = idMap[end_index][1]
+                unused_index = unused_layers.pop(0)
+                end_index -= 1
+            else:
+                if idMap[end_index][0] in refited_items:
+                    refited_items[idMap[end_index][0]].append(end_index) 
+                else:
+                    refited_items[idMap[end_index][0]] = [end_index]
+                end_index -= 1
+
+        # Apply
+        for i in range(len(idArr)):
+            for j in range(len(idArr[i])):
+                if idMap[idArr[i][j]][0] != -1:
+                    idArr[i][j] = idMap[idArr[i][j]][0]
+
+        # Cleanup
+        dict_last_index = list(idMap.keys())[-1]
+        for x in range((dict_last_index - num_unused + 1), dict_last_index + 1):
+            del idMap[x]
+        return [idMap, idArr]
+    #Testing functions
+    def targetScan(self,idArr,target):
+        for i in range(len(idArr)):
+            for j in range(len(idArr[i])):
+                if idArr[i][j] == target: 
+                    return [i,j]
+        return False
+    def idMapScan(self,idMap):
+        for i in idMap:
+            print(i,idMap[i][0])
+    def idMapCompare(self,idMap,idMap2):
+        for i in idMap:
+            if idMap[i][0] != idMap2[i][0]:
+                print(i,":",idMap[i][0],"vs",idMap2[i][0])
+    # returns 2D array where each rgb pixel is represented by an id number dictating which color block it belongs to
     def __getID(self, image, x: int, y: int) -> list:
         numArr = np.array(image)  # converts image object to numpy array
         idArr = []
         idMap = {
-            # 0: [0, 0, 0, 0],  # jpeg is length 3 while png is length 4
-            # 1: [0, 0, 0, 255],  # outline of image (for black solid outline though)
+            # 0: [-1,[0, 0, 0, 0]],  # jpeg is length 3 while png is length 4
+            # 1: [-1,[0, 0, 0, 255]],  # outline of image (for black solid outline though)
         }
-
         for i in range(x):
-            idArr.append([])
+            idArr.append([])  # append new row
             for j in range(y):
                 pixel = np.array(numArr[i][j]).tolist()
 
-                if list(idMap):
-                    if self.__getIDHelper(idMap, pixel) is not None:
-                        idArr[i].append(self.__getIDHelper(idMap, pixel))
-                    else:
-                        # new color has been visited -> create new ID
-                        newID = list(idMap)[-1] + 1
-                        idMap[newID] = pixel
-                        idArr[i].append(newID)
-                else:
-                    idMap[0] = pixel
-                    idArr[i].append(0)
+                [layer_num, idMap] = self.getLayerNum(pixel, self.getNeighbors(i, j, idArr), idMap, idArr)
+                idArr[i].append(layer_num)  # append new column pixel
 
+        self.collapse_refit(idMap, idArr)
         self.idColorMap = idMap
         return idArr
 
-    # gets ID for visited pixel
-    def __getIDHelper(self, idMap: dict, pixelToCheck: list) -> int:
-        for colorID in idMap:
-            matches = 0
-            for i in range(len(pixelToCheck)):
-                # checks to see if any rgb value pairs differ by 20 or more
-                if abs(pixelToCheck[i] - idMap[colorID][i]) < 60:
-                    matches += 1
-            if matches == len(pixelToCheck):
-                return colorID
-        return None
+    def getNeighbors(self, i, j, arr):
+        neighbors = []
+        x = len(arr[0])
+
+        if j > 0:
+            neighbors.append(arr[i][j - 1])
+        if i > 0:
+            neighbors.append(arr[i - 1][j])
+            if j > 0:
+                neighbors.append(arr[i - 1][j - 1])
+            if j < x - 1:
+                neighbors.append(arr[i - 1][j + 1])
+        return neighbors
 
     def getDimensions(self) -> tuple:
         if self.__img:
@@ -163,14 +247,13 @@ class Algorithm:
             print("Image deleted")
         except FileNotFoundError:
             print(f"Could not find the file {self.__grayscalePath} to delete")
-			
 
-    def updateExport(self) -> None: # Clear files and export
+    def updateExport(self) -> None:  # Clear files and export
         self.purgeSaved()
         self.export()
 
-    def purgeSaved(self) -> None: # Clear files
-        directory  = "./exports/" + str(self.__id)
+    def purgeSaved(self) -> None:  # Clear files
+        directory = "./exports/" + str(self.__id)
         if os.path.isdir(directory):
             if os.path.isfile(directory + "/title.txt"):
                 os.remove(directory + "/title.txt")
@@ -185,9 +268,9 @@ class Algorithm:
             if os.path.isfile(directory + "/numLayer.txt"):
                 os.remove(directory + "/numLayer.txt")
 
-    def export(self) -> None: # Export files and create directory
-        directory  = "./exports/" + str(self.__id)
-        try: 
+    def export(self) -> None:  # Export files and create directory
+        directory = "./exports/" + str(self.__id)
+        try:
             os.makedirs(directory)
         except OSError:
             if not os.path.isdir(directory):
@@ -198,19 +281,19 @@ class Algorithm:
         self.saveidArrTxt()
         self.saveidColorMapTxt()
         self.saveNumLayerTxt()
-	
+
     def saveTitleTxt(self) -> None:
         txtFile = "./exports/" + str(self.__id) + "/title.txt"
         if not os.path.isfile(txtFile):
             with open(txtFile, "w") as appen:
-                appen.write(str(self.__path.split(".")[1].split("/")[2]))
-			
+                appen.write(str(self.__path.split(".")[-1].split("/")[-1]))
+
     def saveColumnTxt(self) -> None:
         txtFile = "./exports/" + str(self.__id) + "/column.txt"
         if not os.path.isfile(txtFile):
             with open(txtFile, "w") as appen:
                 appen.write(str(self.getDimensions()[1]))
-			
+
     def saveRowTxt(self) -> None:
         txtFile = "./exports/" + str(self.__id) + "/row.txt"
         if not os.path.isfile(txtFile):
@@ -226,9 +309,9 @@ class Algorithm:
         txtFile = "./exports/" + str(self.__id) + "/colorMap.txt"
         if not os.path.isfile(txtFile):
             for target, values in self.idColorMap.items():
-                a = np.array(values)
+                a = np.array(values[1])
                 with open(txtFile, "ab") as appen:
-                    np.savetxt(appen, a.reshape(1,a.shape[0]), fmt="%d", delimiter=",")
+                    np.savetxt(appen, a.reshape(1, a.shape[0]), fmt="%d", delimiter=",")
 
     def saveNumLayerTxt(self) -> None:
         txtFile = "./exports/" + str(self.__id) + "/numLayer.txt"
@@ -243,7 +326,7 @@ class Algorithm:
         for i in range(len(idArr)):
             convertedID.append([])
             for j in range(len(idArr[i])):
-                convertedID[i].append(self.idColorMap[idArr[i][j]])
+                convertedID[i].append(self.idColorMap[idArr[i][j]][1])
         return np.array(convertedID)
 
     # returns color has map with color ID and their corresponding pixel(rgb) list
@@ -251,6 +334,13 @@ class Algorithm:
         return self.idColorMap
 
 
+ball = Algorithm("./ball.jpg", 3)
+ball.export()
+plt.imshow(ball.idToRGB())
+plt.show()
+
+
+'''
 # Get product names that were done before
 items = []
 with open("items.txt", "r") as f:
@@ -298,3 +388,4 @@ print("End List:", items)
 with open("items.txt", "w") as f:
     for s in items:
         f.write(str(s) +"\n")
+'''
